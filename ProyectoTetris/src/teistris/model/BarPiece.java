d6c84c9a79e30a9b9c882812a91192f91add631e
mateo.alfayafontan <mateo.alfayafontan@daw1-32.dip.local>
2025-01-15 18:09:07 +0100

    Acabada parte 2

--- ProyectoTetris/src/teistris/model/BarPiece.java
+++ ProyectoTetris/src/teistris/model/BarPiece.java
@@ -2,8 +2,11 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package teistris;
+package teistris.model;
 
+import teistris.model.Piece;
+import teistris.model.Square;
+import teistris.model.Game;
 import java.awt.Color;
 
 /**
--- ProyectoTetris/src/teistris/model/Game.java
+++ ProyectoTetris/src/teistris/model/Game.java
@@ -14,8 +14,9 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-package teistris;
+package teistris.model;
 
+import teistris.view.MainWindow;
 import java.awt.RenderingHints;
 import java.util.HashMap;
 
