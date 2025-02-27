7ddb63736de2041a2bda1e04b86373e16cba281a
XaquinAlves <xaquinalves@gmail.com>
2025-02-27 12:34:17 +0100

    Minnor code review (model)

--- ProyectoTetris/src/teistris/model/BagOfPieces.java
+++ ProyectoTetris/src/teistris/model/BagOfPieces.java
@@ -18,7 +18,6 @@ import teistris.model.extendedpieces.DotSquareInvertedPiece;
 import teistris.model.extendedpieces.DotSquarePiece;
 import teistris.model.extendedpieces.FInvertedPiece;
 import teistris.model.extendedpieces.FPiece;
-//TODO: fillBagExtended
 
 /**
  * Esta clase xenera unha pila con X pezas aleatorias sen repeticion
@@ -28,7 +27,8 @@ import teistris.model.extendedpieces.FPiece;
 public class BagOfPieces {
 
     /**
-     * Obten unha pila de pezas aleatorias sen repeticion
+     * Obten unha pila de pezas aleatorias sen repeticion, coas pezas do
+     * tetris clásico
      *
      * @param game referencia a partida actual
      * @return pila de pezas
@@ -39,26 +39,13 @@ public class BagOfPieces {
 
         for (int i = 0; i < piecesIds.length; i++) {
             switch (piecesIds[i]) {
-                case 0:
-                    pieces.add(new SquarePiece(game));
-                    break;
-                case 1:
-                    pieces.add(new BarPiece(game));
-                    break;
-                case 2:
-                    pieces.add(new LPiece(game));
-                    break;
-                case 3:
-                    pieces.add(new TPiece(game));
-                    break;
-                case 4:
-                    pieces.add(new LInvertedPiece(game));
-                    break;
-                case 5:
-                    pieces.add(new ZPiece(game));
-                    break;
-                case 6:
-                    pieces.add(new ZInvertedPiece(game));
+                case 0 -> pieces.add(new SquarePiece(game));
+                case 1 -> pieces.add(new BarPiece(game));
+                case 2 -> pieces.add(new LPiece(game));
+                case 3 -> pieces.add(new TPiece(game));
+                case 4 -> pieces.add(new LInvertedPiece(game));
+                case 5 -> pieces.add(new ZPiece(game));
+                case 6 -> pieces.add(new ZInvertedPiece(game));
             }
         }
 
@@ -66,7 +53,8 @@ public class BagOfPieces {
     }
 
     /**
-     * Obten unha pila de pezas aleatorias sen repeticion
+     * Obten unha pila de pezas aleatorias sen repeticion, con pezas extra
+     * respecto ao tetris orixinal
      *
      * @param game referencia a partida actual
      * @return pila de pezas
@@ -77,51 +65,21 @@ public class BagOfPieces {
 
         for (int i = 0; i < piecesIds.length; i++) {
             switch (piecesIds[i]) {
-                case 0:
-                    pieces.add(new SquarePiece(game));
-                    break;
-                case 1:
-                    pieces.add(new BarPiece(game));
-                    break;
-                case 2:
-                    pieces.add(new LPiece(game));
-                    break;
-                case 3:
-                    pieces.add(new TPiece(game));
-                    break;
-                case 4:
-                    pieces.add(new LInvertedPiece(game));
-                    break;
-                case 5:
-                    pieces.add(new ZPiece(game));
-                    break;
-                case 6:
-                    pieces.add(new ZInvertedPiece(game));
-                    break;
-                case 7:
-                    pieces.add(new CrossPiece(game));
-                    break;
-                case 8:
-                    pieces.add(new BigLPiece(game));
-                    break;
-                case 9:
-                    pieces.add(new BigLInvertedPiece(game));
-                    break;
-                case 10:
-                    pieces.add(new BridgePiece(game));
-                    break;
-                case 11:
-                    pieces.add(new DotSquareInvertedPiece(game));
-                    break;
-                case 12:
-                    pieces.add(new DotSquarePiece(game));
-                    break;
-                case 13:
-                    pieces.add(new FPiece(game));
-                    break;
-                case 14:
-                    pieces.add(new FInvertedPiece(game));
-                    break;
+                case 0 -> pieces.add(new SquarePiece(game));
+                case 1 -> pieces.add(new BarPiece(game));
+                case 2 -> pieces.add(new LPiece(game));
+                case 3 -> pieces.add(new TPiece(game));
+                case 4 -> pieces.add(new LInvertedPiece(game));
+                case 5 -> pieces.add(new ZPiece(game));
+                case 6 -> pieces.add(new ZInvertedPiece(game));
+                case 7 -> pieces.add(new CrossPiece(game));
+                case 8 -> pieces.add(new BigLPiece(game));
+                case 9 -> pieces.add(new BigLInvertedPiece(game));
+                case 10 -> pieces.add(new BridgePiece(game));
+                case 11 -> pieces.add(new DotSquareInvertedPiece(game));
+                case 12 -> pieces.add(new DotSquarePiece(game));
+                case 13 -> pieces.add(new FPiece(game));
+                case 14 -> pieces.add(new FInvertedPiece(game));
             }
         }
 
@@ -129,13 +87,14 @@ public class BagOfPieces {
     }
 
     /**
-     * Xera un array de enteiros e desordenaos, usado no metodo fillBag
+     * Xera un array de enteiros e desordenaos, usado nos metodos fillBag
      *
      * @param numberOfPieces cantidade de enteiros( xerase de 0 a este)
      * @return lista de enteiros desordenada
      */
     private static int[] generateBag(int numberOfPieces) {
-        int[] pieces = IntStream.rangeClosed(0, numberOfPieces).toArray();
+        int[] pieces = IntStream.rangeClosed(0,
+                numberOfPieces).toArray();
         //desordenando los elementos
         Random r = new Random();
         for (int i = pieces.length; i > 0; i--) {
